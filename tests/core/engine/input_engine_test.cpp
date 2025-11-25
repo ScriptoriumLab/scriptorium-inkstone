0b94443d92b9d3709374a20e4f5f216fa1d1eb67
Edward NoaLand <edwardnoaland@gmail.com>
2025-11-25 21:23:37 +0000

    refactor: move post build to an isolated target instead of a command

--- tests/core/engine/input_engine_test.cpp
+++ tests/core/engine/input_engine_test.cpp
@@ -6,7 +6,7 @@
 
 TEST(pinyin_engine_test, should_successfully_load_dictionary_and_show_candidates_when_input_correct_pinyin) {
 	auto engine = modian::inkstone::core::pinyin_engine{};
-	// engine.load_dictionary(std::string{PROJECT_SOURCE_DIR}.append("/data/pinyin_dictionary.txt"));
+	engine.load_dictionary(std::string{PROJECT_SOURCE_DIR}.append("/data/pinyin_dictionary.txt"));
 
 	const auto candidates = engine.convert(L"ni");
 
