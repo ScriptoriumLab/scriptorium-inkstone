02345b68a54be24fa09352adc8f5f03d422aa5d4
Edward NoaLand <edwardnoaland@gmail.com>
2026-02-04 23:37:28 +0000

    refactor: fix logger info error and debug throw clangd error

--- core/include/modian/core/logger/base_logger.h
+++ core/include/modian/core/logger/base_logger.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <string_view>
 #include <format>
+#include <type_traits>
 
 namespace modian::inkstone::core {
 
@@ -15,33 +16,37 @@ namespace modian::inkstone::core {
         [[nodiscard]] virtual std::string_view type() const { return "base_logger"; }
 
         template<typename... Args>
-        void info(std::format_string<Args...> fmt, Args&&... args) {
-            format_and_sink(log_level::info, fmt, std::forward<Args>(args)...);
+        requires (sizeof...(Args) > 0)
+        void info(std::format_string<std::type_identity_t<Args>...> fmt, Args&&... args) {
+            sink_formatted(log_level::info, fmt.get(), std::make_format_args(args...));
         }
 
         void info(std::string_view msg)  { sink_it(log_level::info, msg); }
 
         template<typename... Args>
+        requires (sizeof...(Args) > 0)
         void debug(std::format_string<Args...> fmt, Args&&... args) {
-            format_and_sink(log_level::debug, fmt, std::forward<Args>(args)...);
+            sink_formatted(log_level::debug, fmt.get(), std::make_format_args(args...));
         }
 
         void debug(std::string_view msg) { sink_it(log_level::debug, msg); }
 
         template<typename... Args>
+        requires (sizeof...(Args) > 0)
         void error(std::format_string<Args...> fmt, Args&&... args) {
-            format_and_sink(log_level::error, fmt, std::forward<Args>(args)...);
+            sink_formatted(log_level::error, fmt.get(), std::make_format_args(args...));
         }
 
         void error(std::string_view msg) { sink_it(log_level::error, msg); }
 
     private:
-        template<typename... Args>
-        void format_and_sink(log_level level, std::format_string<Args...> fmt, Args&&... args) {
+        void sink_formatted(log_level level, std::string_view fmt, const std::format_args& args) {
             try {
-                std::string formatted = std::format(fmt, std::forward<Args>(args)...);
+                std::string formatted = std::vformat(std::string_view(fmt), args);
+
                 sink_it(level, formatted);
-            } catch (const std::format_error& e) {
+            } catch (const std::exception& e) {
+                // 安全的 fallback
                 sink_it(log_level::error, std::string("[Logger Error] Format failed: ") + e.what());
             }
         }
