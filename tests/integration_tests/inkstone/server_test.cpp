89e5e7038b1a4aa2e6776646de698ad087ab0c90
Edward NoaLand <edwardnoaland@gmail.com>
2026-02-06 15:16:15 +0000

    refactor & test: extract server as lib and add integration test cases for server

---
+++ tests/integration_tests/inkstone/server_test.cpp
@@ -0,0 +1,77 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include <chrono>
+
+#include "modian/core/engine/input_engine.h"
+#include "modian/core/engine/pinyin_engine.h"
+
+#include "modian/core/logger/logger_service.h"
+#include "modian/logger/spdlog_logger.h"
+#include "../../inkstone/server.h"
+
+#include "include/modian/tests/utils/test_pipe_client.h"
+#include "modian/infra/utils/utils.h"
+
+using namespace std::chrono_literals;
+
+class server_integration_tests : public ::testing::Test {
+protected:
+	static void SetUpTestSuite() {
+		modian::inkstone::core::logger_service::update_logger([](){
+			return std::make_shared<modian::inkstone::infra::logger::spdlog_logger>();
+		});
+	}
+
+	void SetUp() override {
+		auto dict_loader = modian::inkstone::core::lazy_load_dictionary<modian::inkstone::core::pinyin_engine>(std::string{PROJECT_SOURCE_DIR}.append("/data/pinyin_dictionary.txt"));
+
+		server_instance_ = std::make_unique<modian::inkstone::server>(dict_loader);
+
+		server_thread_ = std::jthread([this]() {
+			server_instance_->run();
+		});
+
+		std::this_thread::sleep_for(200ms);
+	}
+
+	void TearDown() override {
+		if (server_instance_) {
+			server_instance_->signal_stop();
+		}
+	}
+
+	std::unique_ptr<modian::inkstone::server> server_instance_;
+	std::jthread server_thread_;
+	const std::string PIPE_NAME = R"(\\.\pipe\modian_ipc_brush)";
+};
+
+TEST_F(server_integration_tests, should_successfully_return_candidate_when_input_is_correct) {
+	modian::inkstone::tests::utils::test_pipe_client client(PIPE_NAME);
+	ASSERT_TRUE(client.connect()) << "Failed to connect to server";
+
+	const auto response_without_candidates = client.send_and_receive("n");
+	EXPECT_THAT(response_without_candidates, testing::HasSubstr("U:n"));
+
+	const auto response_with_candidates = client.send_and_receive("i");
+	EXPECT_THAT(response_with_candidates, testing::HasSubstr("C:"));
+	EXPECT_THAT(response_with_candidates, testing::HasSubstr("你"));
+}
+
+TEST_F(server_integration_tests, should_successfully_handle_backspace_when_user_input_is_correct) {
+	modian::inkstone::tests::utils::test_pipe_client client(PIPE_NAME);
+	ASSERT_TRUE(client.connect());
+
+	client.send_and_receive("d");
+	client.send_and_receive("i");
+	client.send_and_receive("a");
+	const auto response_after_backspace = client.send_and_receive("\b");
+	EXPECT_THAT(response_after_backspace, testing::HasSubstr("U:di"));
+
+	client.send_and_receive("a");
+	const auto response_with_candidates = client.send_and_receive("n");
+	EXPECT_THAT(response_with_candidates, testing::HasSubstr("点"));
+
+	const auto empty_text = client.send_and_receive("\b");
+	ASSERT_TRUE(empty_text.empty());
+}
---
+++ tests/utils/include/modian/tests/utils/test_pipe_client.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <filesystem>
+#include <chrono>
+
+#include "src/gtest-internal-inl.h"
+
+using namespace std::chrono_literals;
+
+namespace modian::inkstone::tests::utils {
+	class test_pipe_client {
+	public:
+		explicit test_pipe_client(std::string_view pipe_name) {
+			pipe_path_ = std::filesystem::path(pipe_name).wstring();
+		}
+
+		~test_pipe_client() {
+			close();
+		}
+
+		bool connect(int max_retries = 5) {
+			for (int i = 0; i < max_retries; ++i) {
+				handle_ = CreateFileW(
+					pipe_path_.c_str(),
+					GENERIC_READ | GENERIC_WRITE,
+					0, nullptr, OPEN_EXISTING, 0, nullptr
+                );
+
+				if (handle_ != INVALID_HANDLE_VALUE) {
+					DWORD mode = PIPE_READMODE_MESSAGE;
+					SetNamedPipeHandleState(handle_, &mode, nullptr, nullptr);
+					return true;
+				}
+
+				if (GetLastError() == ERROR_PIPE_BUSY) {
+					WaitNamedPipeW(pipe_path_.c_str(), 200);
+				} else {
+					std::this_thread::sleep_for(100ms);
+				}
+			}
+			return false;
+		}
+
+		[[nodiscard]] std::string send_and_receive(std::string_view message) const {
+			if (handle_ == INVALID_HANDLE_VALUE) {
+				throw std::runtime_error("Pipe not connected");
+			}
+
+			DWORD bytes_written;
+			if (!WriteFile(handle_, message.data(), static_cast<DWORD>(message.size()), &bytes_written, nullptr)) {
+				throw std::runtime_error("Failed to write to pipe");
+			}
+
+			std::vector<char> buffer(1024 * 16);
+			DWORD bytes_read;
+			if (!ReadFile(handle_, buffer.data(), static_cast<DWORD>(buffer.size()), &bytes_read, nullptr)) {
+				throw std::runtime_error("Failed to read from pipe");
+			}
+
+			return { buffer.data(), bytes_read };
+		}
+
+		void close() {
+			if (handle_ != INVALID_HANDLE_VALUE) {
+				CloseHandle(handle_);
+				handle_ = INVALID_HANDLE_VALUE;
+			}
+		}
+	private:
+		std::wstring pipe_path_;
+		HANDLE handle_{INVALID_HANDLE_VALUE};
+	};
+}
