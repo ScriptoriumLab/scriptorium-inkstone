5d890addefc1c2d330299a930de98b6dc14fe2ab
Edward NoaLand <edwardnoaland@gmail.com>
2026-02-08 18:25:39 +0000

    feat: introduce key_event protocol in core layer

---
+++ core/include/modian/core/protocol/key_event.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+namespace modian::inkstone::core::protocol::input::v1 {
+	class key_event {
+	public:
+		explicit key_event(std::string content);
+	private:
+		std::string content;
+	};
+}
