df910e0bcb9befa6ffe73b110cb68553f98b12e5
Edward NoaLand <edwardnoaland@gmail.com>
2026-02-08 16:31:16 +0000

    refactor: move message type out of instruction

--- core/include/modian/core/protocol/instruction.h
+++ core/include/modian/core/protocol/instruction.h
@@ -3,14 +3,14 @@
 #include <string>
 
 namespace modian::inkstone::core::protocol::input::v1 {
+	enum class message_type {
+		NONE = 'N',
+		UPDATE = 'U',
+		COMMIT = 'C'
+	};
+
 	class instruction {
 	public:
-		enum class message_type {
-			NONE = 'N',
-			UPDATE = 'U',
-			COMMIT = 'C'
-		};
-
 		instruction();
 		instruction(const message_type& type, std::string payload);
 
