4b63ab8cbe384403dc695eecebd50abaa540a510
Edward NoaLand <edwardnoaland@gmail.com>
2026-04-19 17:46:50 +0100

    refactor: add engine plugin concepts for lazy load dictionary

--- core/include/modian/core/engine/input_engine.h
+++ core/include/modian/core/engine/input_engine.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <vector>
 #include <string>
 #include <functional>
@@ -18,7 +19,12 @@ namespace modian::inkstone::core {
 		[[nodiscard]] virtual std::string get_raw_input() const = 0;
 	};
 
-	template<typename T>
+    template<typename T>
+    concept engine_plugin = std::derived_from<T, input_engine> && requires {
+         { T::id } -> std::convertible_to<std::string_view>;
+     };
+
+	template<engine_plugin T>
 	std::pair<std::string, std::function<std::shared_ptr<input_engine>()>> lazy_load_dictionary(const std::string& dictionary_path) {
 		return std::make_pair(std::string{T::id}, [dictionary_path]() -> std::shared_ptr<input_engine> { return std::make_shared<T>(dictionary_path); });
 	}
