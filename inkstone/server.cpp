94a72ae3734a8abdebb1dfffbbe1a397993c9544
Edward NoaLand <edwardnoaland@gmail.com>
2026-04-21 23:03:28 +0100

    refactor: add TODO for clicking candidate from UI

--- inkstone/server.cpp
+++ inkstone/server.cpp
@@ -50,6 +50,7 @@ namespace modian::inkstone {
 
 				const auto text = session_orchestrator_->select_candidate(index);
 				if (!text.empty()) {
+                    // TODO: need to solve async select candidates (i.e., clicking one candidate from UI need to notify inkstone and brush to replace with this candidate)
                     core::logger_service::logger()->info("TODO: Commit text to Brush: {}", text);
 				}
 			}
