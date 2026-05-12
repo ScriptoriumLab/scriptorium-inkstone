9dcd0771a567c337a79b7711785b34bcb6aaf8b1
Edward NoaLand <edwardnoaland@gmail.com>
2026-05-12 23:27:27 +0100

    refactor: move logger_guard into namespace

--- inkstone/app.cpp
+++ inkstone/app.cpp
@@ -15,7 +15,7 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 #endif
-    logger_guard<modian::common::infra::logger::spdlog_logger> log_guard{};
+    modian::inkstone::logger_guard<modian::common::infra::logger::spdlog_logger> log_guard{};
 
     try {
         modian::inkstone::server inkstone_server{
--- inkstone/logger_guard.h
+++ inkstone/logger_guard.h
@@ -2,21 +2,22 @@
 
 #include "modian/common/core/logger/logger_service.h"
 
-// TODO: add namespace to logger guard
-template<typename logger>
-struct logger_guard {
-    logger_guard() {
-        modian::common::core::logger_service::update_logger([](){
-            return std::make_shared<logger>();
-        });
-    }
+namespace modian::inkstone {
+    template<typename logger>
+    struct logger_guard {
+        logger_guard() {
+            common::core::logger_service::update_logger([](){
+                return std::make_shared<logger>();
+            });
+        }
 
-    logger_guard(const logger_guard&) = delete;
-    logger_guard(logger_guard&&) = delete;
-    logger_guard operator=(const logger_guard&) = delete;
-    logger_guard operator=(logger_guard&&) = delete;
+        logger_guard(const logger_guard&) = delete;
+        logger_guard(logger_guard&&) = delete;
+        logger_guard operator=(const logger_guard&) = delete;
+        logger_guard operator=(logger_guard&&) = delete;
 
-    ~logger_guard() {
-        modian::common::core::logger_service::shutdown();
-    }
-};
+        ~logger_guard() {
+            common::core::logger_service::shutdown();
+        }
+    };
+}
