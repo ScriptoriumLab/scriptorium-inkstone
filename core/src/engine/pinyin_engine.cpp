fb6c74cfe4fe445f37114831664587990a116447
Edward NoaLand <edwardnoaland@gmail.com>
2026-05-21 14:46:54 +0100

    feat: support assemble sentences instead only find one character

--- core/src/engine/pinyin_engine.cpp
+++ core/src/engine/pinyin_engine.cpp
@@ -29,13 +29,49 @@ namespace modian::inkstone::core {
 		input_buffer_.clear();
 	}
 
+    namespace {
+        void backtrace_assemble_sentences(const std::vector<std::vector<std::string>>& candidate_matrix, int starter, std::string& sentence, std::vector<std::string>& sentences) {
+            if (starter == candidate_matrix.size()) {
+                sentences.push_back(sentence);
+                return;
+            }
+
+            for (const auto& word : candidate_matrix[starter]) {
+                auto old_length = sentence.size();
+                sentence.append(word);
+
+                backtrace_assemble_sentences(candidate_matrix, starter + 1, sentence, sentences);
+
+                sentence.resize(old_length);
+            }
+        }
+    }
+
 	std::vector<std::string> pinyin_engine::get_candidates() const {
 		if (input_buffer_.empty()) {
 			return {};
 		}
 
-        auto tokens = tokenizer_.split(input_buffer_);
-        return dictionary_.lookup(input_buffer_);
+        auto spelling_paths = tokenizer_.split(input_buffer_);
+        if (spelling_paths.empty()) {
+            return {};
+        }
+
+        std::vector<std::string> final_candidates;
+        for (const auto& path : spelling_paths) {
+            std::vector<std::vector<std::string>> candidate_matrix;
+            for (const auto& spelling : path) {
+                candidate_matrix.push_back(dictionary_.lookup(spelling));
+            }
+
+            std::vector<std::string> sentences;
+            std::string sentence;
+            backtrace_assemble_sentences(candidate_matrix, 0, sentence, sentences);
+
+            final_candidates.insert(final_candidates.end(), sentences.begin(), sentences.end());
+        }
+
+        return final_candidates;
 	}
 
 	[[nodiscard]] std::string pinyin_engine::get_raw_input() const {
