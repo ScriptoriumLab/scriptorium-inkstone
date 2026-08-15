5d2d9849f210959db56ee3d31e5fffde2c927d17
Edward NoaLand <edwardnoaland@gmail.com>
2026-08-15 13:32:43 +0100

    test: test if engine will return correct spelling path when input is correct

--- tests/integration_tests/core/engine/input_engine_test.cpp
+++ tests/integration_tests/core/engine/input_engine_test.cpp
@@ -16,3 +16,17 @@ TEST(pinyin_engine_test, should_successfully_load_dictionary_and_show_candidates
 	ASSERT_EQ(candidates.at(1).word, std::string{"尼"});
 	ASSERT_EQ(candidates.at(2).word, std::string{"泥"});
 }
+
+TEST(pinyin_engine_test, should_successfully_load_dictionary_and_has_correct_spelling_path_when_input_correct_pinyin) {
+	auto engine = scriptorium::inkstone::core::pinyin_engine{std::string{PROJECT_SOURCE_DIR}.append("/data/pinyin_dictionary.txt")};
+
+	engine.update_input_state('n');
+	engine.update_input_state('i');
+    engine.update_input_state('h');
+    engine.update_input_state('a');
+    engine.update_input_state('o');
+	const auto candidates = engine.get_candidates();
+
+    ASSERT_EQ(candidates.at(0).spelling_path.at(0), "ni");
+    ASSERT_EQ(candidates.at(0).spelling_path.at(1), "hao");
+}
